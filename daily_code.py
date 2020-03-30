4434c462ad6093dc65d3dcf0d3d8eab79192692f
ECNUHP <39453964+ECNUHP@users.noreply.github.com>
2020-03-30 15:26:28 +0800

    Update daily_code.py

--- daily_code.py
+++ daily_code.py
@@ -278,7 +278,30 @@ print(split_chinese_sentence(text))
 os.path.join(os.path.dirname(__file__),'model/esim_48.ckpt')
 
 
+# 利用正则匹配找到字字符串的位置：
+
+import re
 
 
+def get_match_spans(pattern, input):
+    """
+    Given string pattern and string input,
+    return list of [) char position tuples of patterns in input.
+    :param pattern: string pattern to match.
+    :param input: string input where we find pattern.
+    :return: a list of pattern char position tuples in input.
+    """
+    spans = []
+    for match in re.finditer(re.escape(pattern), input):
+        spans.append(match.span())
+    return spans
 
 
+if __name__ == "__main__":
+    # test get_match_spans
+    pattern = "are"
+    input = "how are you are you"
+    print(get_match_spans(pattern, input))
+    # we will get [(4, 7), (12, 15)]
+
+============================================================================
