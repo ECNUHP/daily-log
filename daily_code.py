b5b68c9ab683c10a43d0aa215b627885534d6f2f
ECNUHP <39453964+ECNUHP@users.noreply.github.com>
2020-04-02 00:23:20 +0800

    Update daily_code.py

--- daily_code.py
+++ daily_code.py
@@ -305,3 +305,37 @@ if __name__ == "__main__":
     # we will get [(4, 7), (12, 15)]
 
 ============================================================================
+
+#计算编辑距离：
+
+
+def minDistance( word1: str, word2: str) -> int:
+    n1 = len(word1)
+    n2 = len(word2)
+    dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+    # 第一行
+    for j in range(1, n2 + 1):
+        dp[0][j] = dp[0][j-1] + 1
+    # 第一列
+    for i in range(1, n1 + 1):
+        dp[i][0] = dp[i-1][0] + 1
+    for i in range(1, n1 + 1):
+        for j in range(1, n2 + 1):
+            if word1[i-1] == word2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1] ) + 1
+    #print(dp)
+    return dp[-1][-1]
+
+
+#四舍五入保留四位小数
+res=0.8
+res=('%.4f'%res)
+print(res)
+
+
+
+
+
+
